df7d6e610cc0abbd7e11d6fc63fd03601b52ad36
Keiane Balicanta <74569357+keiane@users.noreply.github.com>
2024-02-01 12:42:30 -0500

    Removed hypertext.py, updated documentation.md

--- hypertext.py
+++
@@ -1,7 +0,0 @@
-# Add gradio HTML tags here
-github_tag = """
-<p1>
-    View the Github repo for this site <a href='https://github.com/keiane/clinic_fractional_calculus/tree/main'>here!</a>    
-</p1>
-"""
-
